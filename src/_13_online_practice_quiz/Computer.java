2d1442e6d18a03968d12d968aa40c147d95fbe61
EJTC2005 <ejtclb2005@gmail.com>
2020-11-30 15:56:39 -0800

    practice quiz and advanced binary converter

---
+++ src/_13_online_practice_quiz/Computer.java
@@ -0,0 +1,14 @@
+package _13_online_practice_quiz;
+
+public class Computer {
+	String type;
+	int ramInMB;
+	public Computer(String type, int ramInMB){
+		this.type = type;
+		this.ramInMB = ramInMB;
+  }
+	public static void main() {
+		Computer i386;
+		
+	}
+}
